3a5e5507ab91e17461800af43b713e84740f31b0
werner <werner.dittmann@t-online.de>
2021-12-03 16:37:15 +0100

    Include cbc encryption mode to amalgamation

--- amalgamation.sh
+++ amalgamation.sh
@@ -3,7 +3,7 @@
 # Script to produce Botan amalgamation for ZRTP project, host (Linux x86_64) library
 
 # Modules required by ZRTP library
-modules="sha2_32,sha2_64,sha1,twofish,aes,skein,hmac,pubkey,curve25519,cfb,ec_group,ecdh,dh,dl_group"
+modules="sha2_32,sha2_64,sha1,twofish,aes,skein,hmac,pubkey,curve25519,cfb,cbc,ec_group,ecdh,dh,dl_group"
 
 # Location of my ZRTP project
 zrtpBaseDir="$HOME/devhome/ZRTPCPP/botancrypto/x86_64"
---
+++ generateCmake.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# used config command line
+./configure.py --with-build-dir=newBuild  --with-cmake --cc=clang --link-method=symlink
+
