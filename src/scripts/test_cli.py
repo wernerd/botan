6eba81970b5693ad3449a8d0bc97004c85b90486
Jack Lloyd <jack@randombit.net>
2022-12-02 16:56:14 -0500

    Remove support for NEWHOPE and CECPQ1

--- src/scripts/test_cli.py
+++ src/scripts/test_cli.py
@@ -1148,8 +1148,8 @@ def cli_speed_pk_tests(_tmp_dir):
     msec = 1
 
     pk_algos = ["ECDSA", "ECDH", "SM2", "ECKCDSA", "ECGDSA", "GOST-34.10",
-                "DH", "DSA", "ElGamal", "Ed25519", "Curve25519", "NEWHOPE", "McEliece",
-                "RSA", "RSA_keygen", "XMSS", "ec_h2c", "Kyber"]
+                "DH", "DSA", "ElGamal", "Ed25519", "Curve25519", "McEliece",
+                "RSA", "RSA_keygen", "XMSS", "ec_h2c"]
 
     output = test_cli("speed", ["--msec=%d" % (msec)] + pk_algos, None).split('\n')
 
